3d0b890f80c424b73db4779b89c34cffd0090076
HITHESH SANKARARAMAN M <123364828+hithesh-sankararaman@users.noreply.github.com>
2026-09-21 18:43:32 +0530

    Add: python-list-comprehension - submission-0

---
+++ Python Coding Interviews/python-list-comprehension/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:
+    new_list=[]
+    for i in range(1,n+1):
+        if i % 2 !=0:
+            new_list.append(i)
+    return new_list
+
+
+# do not modify below this line
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))
