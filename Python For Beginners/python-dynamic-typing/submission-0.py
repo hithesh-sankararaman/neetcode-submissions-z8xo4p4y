76f5f66df49105444edca5de4483d60b8884ee07
HITHESH SANKARARAMAN M <123364828+hithesh-sankararaman@users.noreply.github.com>
2026-09-26 19:18:14 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hi"
+print(type(variable))
+variable = ['hi','hello']
+print(type(variable))
